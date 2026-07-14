f467eaa36a3673c70f8457cc8484ae76cd550f14
Horia Bosoanca <redplayer333hhh@gmail.com>
2026-07-15 01:33:58 +0300

    moving stuff

--- minecraft/servers/vanilla-anticheat/run.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Java\jdk-25.0.3\bin\java.exe" -Xmx15G -Xms8G -jar fabric-server-mc.1.21.5-loader.0.16.14-launcher.1.0.3.jar nogui
--- minecraft/servers/vanilla-anticheat/run.sh
+++
@@ -1,1 +0,0 @@
-java -Xmx15G -Xms8G -jar fabric-server-mc.1.21.5-loader.0.16.14-launcher.1.0.3.jar nogui
