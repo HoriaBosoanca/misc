f8510d144a2162cf9c61573a62af12f8e374e051
Horia Bosoanca <redplayer333hhh@gmail.com>
2026-07-15 01:54:37 +0300

    a

---
+++ minecraft/servers/anticheat/26.2/run.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk-25.0.3\bin\java.exe" -Xmx15G -Xms8G -jar fabric-server-mc.26.2-loader.0.19.3-launcher.1.1.1.jar nogui
---
+++ minecraft/servers/anticheat/26.2/run.sh
@@ -0,0 +1,1 @@
+java -Xmx15G -Xms8G -jar fabric-server-mc.26.2-loader.0.19.3-launcher.1.1.1.jar nogui
